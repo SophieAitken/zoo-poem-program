b511423979737ea6fdcb088a957280dc2b889215
Sophie Aitken <17083@bayfield-high.school.nz>
2020-05-20 11:45:33 +1200

    Add project files.

---
+++ zoo poem/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace zoo_poem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Walking Roung the Zoo\n";
+            repeatVerse();
+            adaptablePart("parrot", "squawked\nAnd winked");
+
+            repeatVerse();
+            adaptablePart("crocodile", "snapped\nIts jaws");
+        }
+        public void repeatVerse()
+        {
+            label1.Text += "\nWalking round the zoo";
+            label1.Text += "\nWhat did I see?";
+        }
+
+        public void adaptablePart(string animal, string actions)
+        {
+            label1.Text += "\na " + animal + " that " + actions + " at me";
+        }
+    }
+}
